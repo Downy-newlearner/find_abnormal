bf31a2a748482b607d17c5da3864e0824c36040c
Downy-newlearner <jdh251425142514@gmail.com>
2024-08-23 02:25:41 +0900

    preprocessing_class에 내용을 채워주세용 -다훈

---
+++ preprocessing_class.py
@@ -0,0 +1,40 @@
+# 전처리 클래스 만들기
+# 멤버 변수로 데이터프레임을 받아서 전처리를 수행하는 클래스를 만들어보자
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+class Preprocessing:
+    def __init__(self, data):
+        self.data = data
+        self.label_encoder = LabelEncoder()
+        
+    
+    def distingush_always_AbNormal(self, data):
+        
+
+
+        return data_same, data_diff #전처리 된 데이터 반환
+    
+    def dam(self, data):
+        
+
+        return data #전처리 된 데이터 반환
+    
+
+    def fill1(self, data):
+        #
+
+        return data #전처리 된 데이터 반환
+    
+    
+    def fill2(self, data):
+
+        return data #전처리 된 데이터 반환
+    
+
+    def autoclave(self, data):
+        #
+
+        return data #전처리 된 데이터 반환
